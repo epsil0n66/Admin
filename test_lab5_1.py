6ac1515736066b88bbb348e996c5e6aa2c8c3abf
Aleksander Slanina <ale.slanina@gmail.com>
2022-04-17 21:47:52 +0500

    lab 5 parallel

---
+++ test_lab5_1.py
@@ -0,0 +1,7 @@
+import pytest
+
+def text_success():
+    assert 1
+
+def test_success2():
+    assert 1
---
+++ test_lab5_3.py
@@ -0,0 +1,7 @@
+import pytest
+
+def text_success():
+    assert 0
+
+def test_success2():
+    assert 1
